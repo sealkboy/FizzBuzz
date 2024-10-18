a17259a32afb884a3642342676bcb8e091293de4
Seal Kevin Boy Abad <sealkevinboyabad@gmail.com>
2024-10-18 23:00:38 +0200

    Añadida lógica para retornar Fizz, Buzz o FizzBuzz en función de la divisibilidad de num por 3 y 5

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -11,7 +11,6 @@ public class FizzBuzz {
 
     public static String fizzBuzz(int num) {
         String numStr = String.valueOf(num);
-
         boolean divisiblePor3 = num % 3 == 0 || numStr.contains("3");
         boolean divisiblePor5 = num % 5 == 0 || numStr.contains("5");
 
@@ -26,3 +25,4 @@ public class FizzBuzz {
         }
     }
 }
+
